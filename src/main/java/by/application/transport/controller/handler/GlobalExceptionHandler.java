9b18290da6dd9231b19147ce06e1c4bdcc370a4d
Maksim Maksimovich <maksvv.m@yandex.by>
2022-07-12 20:12:22 +0300

    Валидация вводимых дат для поиска заявок

--- src/main/java/by/application/transport/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/by/application/transport/controller/handler/GlobalExceptionHandler.java
@@ -10,12 +10,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.beans.PropertyEditorSupport;
+import java.text.Format;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -33,6 +41,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<CustomExceptionResponseDto> handleNoDataFoundException(NoDataFoundException noDataFoundException) {
         return createResponseForCustomException(HttpStatus.NOT_FOUND, noDataFoundException.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<CustomExceptionResponseDto> handleDateTimeParseException(DateTimeParseException dateTimeParseException) {
+        return createResponseForCustomException(HttpStatus.BAD_REQUEST, "the date must be in the format: yyyy-mm-dd");
     }
 
     @Override
@@ -56,4 +69,34 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(customExceptionResponseDto, httpStatus);
     }
 
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.registerCustomEditor(
+                LocalDate.class,
+                new Editor<>(
+                        text -> LocalDate.parse(text, DateTimeFormatter.ISO_LOCAL_DATE),
+                        DateTimeFormatter.ISO_LOCAL_DATE.toFormat()));
+    }
+
+    private static class Editor<T> extends PropertyEditorSupport {
+
+        private final Function<String, T> parser;
+        private final Format format;
+
+        public Editor(Function<String, T> parser, Format format) {
+
+            this.parser = parser;
+            this.format = format;
+        }
+
+        public void setAsText(String text) {
+
+            setValue(this.parser.apply(text));
+        }
+
+        public String getAsText() {
+
+            return format.format((T) getValue());
+        }
+    }
 }
